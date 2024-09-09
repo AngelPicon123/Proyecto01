107931d04496991b15469199dc3affc9b74efd2b
unknown <Notsxba>
2024-09-09 12:31:03 -0500

    Se crearon las funciones Buscar, eliminar y actualizar en Controller Piero Salcedo, Luis lozano y Sebastian Theran

--- Api/src/controller/UserController.php
+++ Api/src/controller/UserController.php
@@ -22,6 +22,28 @@ class UserController{
         echo json_encode($users);
     }
 
+    public function searchUserById() {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+    
+        $data = json_decode(file_get_contents("php://input"));
+    
+        if (!empty($data->id)) {
+            // Llamada al modelo para buscar el usuario por ID
+            $user = $this->model->getUserById($data->id);
+    
+            if ($user) {
+                echo json_encode($user);
+            } else {
+                http_response_code(404);
+                echo json_encode(["message" => "User not found"]);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["message" => "Invalid input"]);
+        }
+    }
+
     public function createUser(){
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
@@ -37,4 +59,44 @@ class UserController{
         }
 
     }
+
+    public function updateUser($id){
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        if (!empty($data->name) && !empty($data->email)) {
+        
+            $updated = $this->model->updateUser($id, $data->name, $data->email);
+
+        if ($updated) {
+            echo json_encode(["message" => "User updated successfully"]);
+        } else {
+            http_response_code(404);
+            echo json_encode(["message" => "User not found"]);
+        }
+    } else {
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid input"]);
+    }
+    }
+
+    public function deleteUser($id) {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: DELETE");
+    
+        if ($this->model->deleteUserById($id)) {
+            echo json_encode([
+                "message" => "User successfully deleted."
+            ]);
+        } else {
+            echo json_encode([
+                "message" => "User could not be deleted."
+            ]);
+        }
+    }
+
+
 }
