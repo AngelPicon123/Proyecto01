d93f7708c968b808dc547e7e5af69f0db77e7aa8
AngelPicon123 <piconsalinasangeldavid@gmail.com>
2024-09-10 09:24:27 -0500

    Actualizacion del UserModels.php se crearon funciones para el CRUD

--- Api/src/models/UserModels.php
+++ Api/src/models/UserModels.php
@@ -7,7 +7,7 @@ class UserModel {
     public function __construct($database) {
         $this->conn = $database;
     }
-
+    //buscar todos los usuarios
     public function getUsers(){
         $query = "SELECT id, nombre, apellido, correo FROM". $this->table_name;
         $stmt = $this->conn->prepare($query);
@@ -15,4 +15,33 @@ class UserModel {
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    //buscar usuarios por ID
+    public function getUserById($id) {
+        $sql = "SELECT * FROM users WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+        //actualizar usuarios
+    public function updateUser($id, $name, $email) {
+        $sql = "UPDATE users SET name = :name, email = :email WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':id', $id);
+    
+        return $stmt->execute();
+    }
+    //ELIMINAR USUARIO
+    public function deleteUserById($id) {
+        $sql = "DELETE FROM users WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->rowCount() > 0;
+    }
+
 }
