26b40e9631f7c821217543c94f690ee74f50e5a6
AngelPicon123 <piconsalinasangeldavid@gmail.com>
2024-09-17 12:31:18 -0500

    En este commit se actualizo lo de pacientes se añadio terapeuta controller y terapeuta model falta corregir y actualizar

--- Api/routes/routes.php
+++ Api/routes/routes.php
@@ -16,50 +16,39 @@ $uri = ltrim($uri, '/');
 $controller = new PacienteController();
 
 switch (true) {
-    //leer todo
+    //leer todos pacientes
     case preg_match('/^pacientes$/', $uri):
         if ($requestMethod === 'GET') {
             $controller->getAllPacientes();
         }
     break;
-    //buscar pot id
+    //buscar paciente por id
     case preg_match('/^pacientes\/searchPacienteById\/(\d+)$/', $uri, $matches):
         if ($requestMethod === 'GET') {
             $id = $matches[1];  // El ID del usuario desde la URL
             $controller->searchPacienteById($id);
         }
         break;
-//crear nuevo
+//crear nuevo paciente
     case preg_match('/^pacientes\/create$/', $uri):
         if ($requestMethod === 'POST') {
             $controller->createPaciente();
         }
         break;
-//actualizar
+//actualizar paciente
     case preg_match('/^pacientes\/updatePaciente\/(\d+)$/', $uri, $matches):
         if ($requestMethod === 'PUT') {
             $id = $matches[1];
             $controller->updatePaciente($id);
         }
         break;
-//borrar
+//borrar paciente
     case preg_match('/^pacientes\/delete\/(\d+)$/', $uri, $matches):
         if ($requestMethod === 'DELETE') {
             $id = $matches[1];
             $controller->deletePaciente($id);
         }
         break;
-
-    case preg_match('/^users/searchUserById/(\d+)$/', $uri, $matches):
-        if ($requestMethod === 'GET') {
-            $id = $matches[1];  // El ID del usuario desde la URL
-            $controller->searchUserById($id);
-        }
-        break;
-
-
-
-
 
     default:
         header("HTTP/1.0 404 NOT FOUND");
