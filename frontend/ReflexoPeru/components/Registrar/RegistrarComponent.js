b7568df947fcc3ad3a27d12a025e0280e5e1762a
ANGELEDU141 <angel.abamto@gmail.com>
2024-09-17 11:53:31 -0500

    fix: :art: POST-ADAPTATION
    Se adapto, la funcion post para los nuevos parametros de la base de datos.

--- frontend/ReflexoPeru/components/Registrar/RegistrarComponent.js
+++ frontend/ReflexoPeru/components/Registrar/RegistrarComponent.js
@@ -25,6 +25,66 @@ export function RegisterUser() {
                     </div>
                 </div>
 
+                <div class="form-row">
+                    <div class="form-group">
+                        <label for="direccion">Dirección</label>
+                        <input type="text" id="direccion" name="direccion">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group">    
+                        <label for="provincia">Provincia</label>                                                                                                                
+                        <select name="provincia" id="provincia">
+                            <option value="Lima">Lima</option>  
+                            <option value="Arequipa">Arequipa</option>
+                            <option value="Piura">Piura</option>
+                            <option value="Tacna">Tacna</option>
+                            <option value="Cajamarca">Cajamarca</option>
+                            <option value="Callao">Callao</option>
+                            <option value="Loreto">Loreto</option>
+                            <option value="Moquegua">Moquegua</option>
+                            <option value="Ayacucho">Ayacucho</option>
+                            <option value="Ancash">Ancash</option>
+                            <option value="Lambayeque">Lambayeque</option>
+                            <option value="Huanuco">Huanuco</option>
+                            <option value="Cusco">Cusco</option>
+                            <option value="Puno">Puno</option>
+                            <option value="Madre de Dios">Madre de Dios</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group">
+                        <label for="distrito">Distrito</label>
+                        <input type="text" id="distrito" name="distrito">
+                    </div>
+                </div>
+                
+                <div class="form-row">
+                    <div class="form-group">
+                        <label for="dni">DNI</label>
+                        <input type="text" id="dni" name="dni">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group">
+                        <label for="sexo">Sexo</label>
+                        <input type="text" id="sexo" name="sexo">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group">
+                        <label for="nroTelefonico">Nro. Telefonico</label>
+                        <input type="text" id="nroTelefonico" name="nroTelefonico">
+                    </div>  
+                </div>
+
+                
+
                 <button type="submit" id="btn-crear" class="btn-submit">Crear Paciente</button>
             </form>
     `;
