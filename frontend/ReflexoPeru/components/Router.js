14baf63ebffc78022e9d3c98d5a3c6f135866d1d
ANGELEDU141 <angel.abamto@gmail.com>
2024-09-18 09:13:26 -0500

    feat: :art: EDIT-FOLDERS-ESTRUCTURE

--- frontend/ReflexoPeru/components/Router.js
+++ frontend/ReflexoPeru/components/Router.js
@@ -1,7 +1,7 @@
 import { PacientsShow } from "./Pacients/PacientsComponent.js";
-import { RegisterUser } from "./Registrar/RegistrarComponent.js";
+import { RegisterUser } from "./Pacients/PacientsComponent.js";
 import { getusers } from "./Pacients/Pacients-get.js";
-import { RegistrarUser } from "./Registrar/RegistrarJ.js";
+import { RegistrarUser } from "./Pacients/Pacients-get.js";
 
 export function Router() {
   let { hash } = location;
